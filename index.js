7255f82be1b76d221035d4f6628eadbd07610a05
UniqueMaharjan <unique.maharzn@gmail.com>
2022-08-03 15:54:39 +0545

    choose background and text color

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 function changeColor(){
-    var element = document.body;
-    element.classList.toggle("dark-mode");
+    // var element = document.body;
+    var color = document.getElementById('color').value;
+    console.log(color);
+    document.body.style.backgroundColor = color; 
+
+    // element.classList.toggle("dark-mode");
+
+}
+function textColor(){
+   var color = document.getElementById('text-color').value;
+   document.body.style.color = color;
 }
